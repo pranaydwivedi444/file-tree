30c04f701707ebd059e891730f4024b85030942a
Pranay <pranaydwivedi32@gmail.com>
2023-03-15 23:29:51 +0530

    DOne

--- src/components/File/File.component.jsx
+++ src/components/File/File.component.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function File({ name, extension }) {
-  return <div>{`${name}${extension}`}</div>;
+  const iconsObject = {
+    mp3: (
+      <img src="https://img.icons8.com/fluency-systems-regular/25/null/mp3.png" />
+    ),
+    png: (
+      <img src="https://img.icons8.com/material-rounded/24/null/picture.png" />
+    ),
+    jpg: (
+      <img src="https://img.icons8.com/material-rounded/24/null/picture.png" />
+    ),
+    docx: (
+      <img src="https://img.icons8.com/color/25/null/google-docs--v1.png" />
+    ),
+    file: <img src="https://img.icons8.com/windows/32/null/happy-file.png" />,
+  };
+
+  const fileIcon = iconsObject[`${extension.slice(1)}`];
+  console.log(fileIcon, `${extension.slice(0, 1)}`);
+
+  return (
+    <div>
+      {fileIcon}
+      {`${name}${extension}`}
+    </div>
+  );
 }
--- src/components/Folder/Folder.component.jsx
+++ src/components/Folder/Folder.component.jsx
@@ -6,9 +6,23 @@ export default function Folder(props) {
   function openClickHandler() {
     setOpen(!open);
   }
+
+  let fileIcon = (
+    <img src="https://img.icons8.com/emoji/24/null/open-file-folder-emoji.png" />
+  );
+
+  if (!open)
+    fileIcon = (
+      <img src="https://img.icons8.com/material-outlined/24/null/folder-invoices--v1.png" />
+    );
   return (
-    <div className="main-folder">
-      <h2 onClick={openClickHandler}>{props.name}</h2>
+    <div>
+      <div className="main-folder">
+        <span onClick={openClickHandler}>
+          {fileIcon}
+          {props.name}{" "}
+        </span>
+      </div>
       {open && <div className="children-container">{props.children}</div>}
     </div>
   );
